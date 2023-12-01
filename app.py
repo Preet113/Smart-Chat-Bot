ca8c0a2f85772259fda82be5afd914ee95b81fb8
Preet Singh Sahota <109590170+Preet113@users.noreply.github.com>
2023-12-01 16:09:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request 
+
+from script import scrape_website
+from script import ask_question
+
+from flask_cors import CORS
+
+
+app = Flask(__name__) 
+
+CORS(app)
+
+@app.route('/scrape_website', methods = ['GET', 'POST',]) 
+def scrape_website_1():
+    url = request.json['url']
+    data = scrape_website(url)
+    return jsonify({'data': data}) 
+
+@app.route('/ask_question', methods = ['GET', 'POST']) 
+def ask_question_1(): 
+    prompt = request.json['prompt']
+    context = request.json['context']
+    data = ask_question(prompt, context)
+    return jsonify({'data': data}) 
+
+# if __name__ == '__main__': 
+
+# 	app.run(debug = True, port=8080)
